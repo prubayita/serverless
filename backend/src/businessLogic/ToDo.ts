8987112dbdd0e0bf014d26eaacdd71e1a8606e8c
prubayita <51474249+prubayita@users.noreply.github.com>
2022-08-08 05:28:07 +0200

    todo

--- backend/src/businessLogic/ToDo.ts
+++ backend/src/businessLogic/ToDo.ts
@@ -1,43 +1,151 @@
-import {TodoItem} from "../models/TodoItem";
-import {parseUserId} from "../auth/utils";
-import {CreateTodoRequest} from "../requests/CreateTodoRequest";
-import {UpdateTodoRequest} from "../requests/UpdateTodoRequest";
-import {TodoUpdate} from "../models/TodoUpdate";
-import {ToDoAccess} from "../dataLayer/ToDoAccess";
+import { TodosAccess } from './todosAcess'
+import { TodoItem } from '../models/TodoItem'
+import { CreateTodoRequest } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import { createLogger } from '../utils/logger'
+import * as uuid from 'uuid'
+import CustomError from '../utils/error';
+import { TodoUpdate } from '../models/TodoUpdate';
+import { getAttachmentUrl } from './attachmentUtils'
+import * as Joi from 'joi'
 
-const uuidv4 = require('uuid/v4');
-const toDoAccess = new ToDoAccess();
 
-export async function getAllToDo(jwtToken: string): Promise<TodoItem[]> {
-    const userId = parseUserId(jwtToken);
-    return toDoAccess.getAllToDo(userId);
+const todoAccess = new TodosAccess();
+
+const logger = createLogger('Todos')
+
+// TODO: Implement businessLogic
+export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
+  try {
+    const result = await todoAccess.getAllTodos(userId);
+    logger.info(`TODO items of user fetched: ${userId}`, JSON.stringify(result))
+    return result;
+  } catch (error) {
+      logger.error('Get Todo Error', error)
+      throw new CustomError(error.message, 500)
+  }
 }
 
-export function createToDo(createTodoRequest: CreateTodoRequest, jwtToken: string): Promise<TodoItem> {
-    const userId = parseUserId(jwtToken);
-    const todoId =  uuidv4();
-    const s3BucketName = process.env.S3_BUCKET_NAME;
+
+export async function createTodo(
+  createTodoRequest: CreateTodoRequest,
+  userId: string
+): Promise<TodoItem> {
+  const todoId = uuid.v4()
+  const valid = createTodoSchema.validate(createTodoRequest);
+  if (valid.error) {
+    logger.error('VALIDATION ERROR', valid.error)
+    throw new CustomError(valid.error.message, 400)
+  }
+  try {
+    const todoItem: TodoItem = {
+      todoId,
+      userId,
+      createdAt: new Date().toISOString(),
+      done: false,
+      attachmentUrl: null,
+      ...createTodoRequest,
+    }
+
+    await todoAccess.createTodo(todoItem)
+    logger.info('Todo created success fully', todoItem)
+    return todoItem;
+  } catch (error) {
+    throw new CustomError(error.message, 500)
+  }
+}
+
+export async function updateTodo(
+  updateTodoRequest: UpdateTodoRequest,
+  todoId: string,
+  userId: string,
+): Promise<TodoItem> {
+
+  const valid = updateTodoSchema.validate(updateTodoRequest);
+  if (valid.error) {
+    logger.error('VALIDATION ERROR', valid.error)
+    throw new CustomError(valid.error.message, 400)
+  }
+
+  try {
+
+    const item = await todoAccess.updateTodo(updateTodoRequest as TodoUpdate, todoId, userId)
+
+    logger.info('TODO updated successfully', {
+      userId,
+      todoId,
+      updated: updateTodo
+    })
+    return item
     
-    return toDoAccess.createToDo({
-        userId: userId,
-        todoId: todoId,
-        attachmentUrl:  `https://${s3BucketName}.s3.amazonaws.com/${todoId}`, 
-        createdAt: new Date().getTime().toString(),
-        done: false,
-        ...createTodoRequest,
-    });
+  } catch (error) {
+    logger.error(error)
+    throw new CustomError(error.message, 500)
+  }
 }
 
-export function updateToDo(updateTodoRequest: UpdateTodoRequest, todoId: string, jwtToken: string): Promise<TodoUpdate> {
-    const userId = parseUserId(jwtToken);
-    return toDoAccess.updateToDo(updateTodoRequest, todoId, userId);
+export async function deleteTodo(
+  todoId: string,
+  userId: string,
+) {
+  try {
+
+    const item = await todoAccess.deleteTodo(todoId, userId)
+    logger.info('TODO deleted successfully', {
+      userId,
+      todoId
+    })
+
+    return item
+  } catch (error) {
+    logger.error(error)
+    throw new CustomError(error.message, 500)
+  }
 }
 
-export function deleteToDo(todoId: string, jwtToken: string): Promise<string> {
-    const userId = parseUserId(jwtToken);
-    return toDoAccess.deleteToDo(todoId, userId);
+export async function generateSignedUrl(attachmentId: string): Promise<string> {
+  try {
+    logger.info('Generating Signed URL')
+    const uploadUrl = await getAttachmentUrl(attachmentId)
+    logger.info('Signed URL generated')
+
+    return uploadUrl
+  } catch (error) {
+    logger.error(error)
+    throw new CustomError(error.message, 500)
+  }
 }
 
-export function generateUploadUrl(todoId: string): Promise<string> {
-    return toDoAccess.generateUploadUrl(todoId);
+export async function updateAttachmentUrl(
+  userId: string,
+  todoId: string,
+  attachmentId: string
+): Promise<void> {
+  try {
+
+    const attachmentUrl = getAttachmentUrl(attachmentId)
+    await todoAccess.updateTodoItemAttachment(userId, todoId, attachmentUrl)
+
+    logger.info(
+      'AttachmentURL updated successfully',{
+        userId,
+        todoId
+      }
+    )
+    return
+  } catch (error) {
+    logger.error(error)
+    throw new CustomError(error.message, 500)
+  }
 }
+
+const createTodoSchema = Joi.object({
+  name: Joi.string().required(),
+  dueDate: Joi.string().required()
+})
+
+const updateTodoSchema = Joi.object({
+  name: Joi.string().required(),
+  dueDate: Joi.string().required(),
+  done: Joi.boolean().required()
+})
